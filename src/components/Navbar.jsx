0736033059e27437da5cbb89f9785524e665e83e
leonardeganow <leonard@eganow.com>
2024-02-05 11:49:33 +0000

    added content to about page(dummy data )

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import { MdOutlineGroups2, MdCastForEducation } from "react-icons/md";
 import { AiOutlineThunderbolt } from "react-icons/ai";
 import { GiHamburger } from "react-icons/gi";
 import { IoLogoTux } from "react-icons/io";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { GiAquarium } from "react-icons/gi";
 
 const Navbar = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
   return (
     <nav className=" flex justify-between  py-3  shadow-xl px-5 bg-[#0c162c]  z-10 w-full">
       <div className="font-bold col-span-4  left-10 text-white top-10 ">
@@ -19,27 +21,51 @@ const Navbar = () => {
       <div className=" col-span-8 flex justify-end sm:justify-start">
         <ul className="md:flex    gap-20 hidden">
           <Link to="/about">
-            <div className="flex flex-col items-center cursor-pointer text-white hover:text-rose-400 hover:border-b-4 pb-2 transition duration-300 ease-in-out">
+            <div
+              className={`flex flex-col items-center cursor-pointer  hover:border-b-4 pb-2 transition duration-300 ease-in-out ${
+                currentUrl === "/about"
+                  ? "border-b-4 text-white border-pink-500"
+                  : "text-white"
+              }`}
+            >
               <LiaInfoSolid size={25} />
               <p className="">About</p>
             </div>
           </Link>
 
           <Link to="/teams">
-            <div className="flex flex-col items-center cursor-pointer text-white hover:text-rose-400 hover:border-b-4 pb-2 transition duration-300 ease-in-out">
+            <div
+              className={`flex flex-col items-center cursor-pointer  hover:border-b-4 pb-2 transition duration-300 ease-in-out ${
+                currentUrl === "/teams"
+                  ? "border-b-4 text-white border-pink-500"
+                  : "text-white"
+              }`}
+            >
               <MdOutlineGroups2 size={25} />
               <p className="">Teams</p>
             </div>
           </Link>
 
           <Link to="/educationhub">
-            <div className="flex flex-col items-center cursor-pointer text-white hover:text-rose-400 hover:border-b-4 pb-2 transition duration-300 ease-in-out">
+            <div
+              className={`flex flex-col items-center cursor-pointer  hover:border-b-4 pb-2 transition duration-300 ease-in-out ${
+                currentUrl === "/educationhub"
+                  ? "border-b-4 text-white border-pink-500"
+                  : "text-white"
+              }`}
+            >
               <MdCastForEducation size={25} />
               <span className="">Education hub</span>
             </div>
           </Link>
           <Link to="/activities">
-            <div className="flex flex-col items-center cursor-pointer text-white hover:text-rose-400 hover:border-b-4 pb-2 transition duration-300 ease-in-out">
+            <div
+              className={`flex flex-col items-center cursor-pointer  hover:border-b-4 pb-2 transition duration-300 ease-in-out ${
+                currentUrl === "/activities"
+                  ? "border-b-4 border-pink-500 text-white"
+                  : "text-white"
+              }`}
+            >
               <AiOutlineThunderbolt size={25} />
               <p className="">Activities</p>
             </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import aboutimageone from "../assets/aboutimage.jpg";
+import aboutimagetwo from "../assets/aboutimagetwo.jpg";
+import { IoWaterOutline } from "react-icons/io5";
+import { GiWaterfall } from "react-icons/gi";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="sm:grid grid-cols-12 h-dvh ">
+      <div
+        style={{ backgroundImage: "url(" + aboutimageone + ")" }}
+        className="col-span-3  bg-cover"
+      ></div>
+      <div className="col-span-9  grid place-items-center bg-[#f0e1c9] py-10 sm:py-0">
+        <div className="w-1/2 text-center">
+          <h1 className="text-5xl font-bold text-pink-400">ABOUT US</h1>
+
+          <p className="text-[#616974] font-semibold py-5">lorem ipsum dilor</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            odit, ipsam, alias voluptates similique suscipit voluptas dolores
+            nulla porro possimus tenetur libero asperiores quod. Voluptate
+            veniam soluta veritatis officia placeat!
+          </p>
+        </div>
+      </div>
+      <div className="col-span-5 bg-[#172b46] grid place-items-center py-10 sm:py-0">
+        <div className="text-center">
+          <div className="flex justify-center">
+            <IoWaterOutline size={40} className="text-pink-400" />
+          </div>
+          <h1 className="text-pink-600 font-black py-3 text-lg">
+            Learn marine science
+          </h1>
+          <p className="w-1/2 mx-auto text-pink-100">
+            Learn with no restrictions a wide range of producsts are now one
+            click away
+          </p>
+        </div>
+      </div>
+      <div className="col-span-7 bg-[#0f1a30] grid place-items-center py-10 sm:py-0">
+        <div className="text-center">
+          <div className="flex justify-center">
+            <GiWaterfall size={40} className="text-pink-400" />
+          </div>
+          <h1 className="text-pink-600 font-black py-3 text-lg">
+            Learn marine science
+          </h1>
+          <p className="w-1/2 mx-auto text-pink-100">
+            Learn with no restrictions a wide range of producsts are now one
+            click away
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
