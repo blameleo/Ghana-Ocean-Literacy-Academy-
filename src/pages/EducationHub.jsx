0fe63c9a2263a44c4298547956386081e5737853
leonardeganow <leonard@eganow.com>
2024-02-07 16:45:45 +0000

    added search bar to educationhub page

--- src/pages/EducationHub.jsx
+++ src/pages/EducationHub.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const EducationHub = () => {
-  return <div>EducationHub</div>;
+  return (
+    <div>
+      <div className="bg-gradient-to-bl from-[#0c162c]  via-[#0c162c] to-pink-500 py-10 relative">
+        <form className=" w-[50%] mx-auto ">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 ps-10 outline-none text-sm text-black  border-gray-300 rounded-lg bg-gray-300  dark:bg-gray-700 dark:border-gray-600 placeholder-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search pdfs"
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-pink-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="h-screen bg-[#0c162c]"></div>
+    </div>
+  );
 };
 
 export default EducationHub;
