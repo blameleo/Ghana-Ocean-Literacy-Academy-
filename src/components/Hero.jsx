33b07d4c0b97da5c7bc1a5db60da08528cfa0c44
Dstvlover <abayekkd@gmail.com>
2024-02-13 15:22:46 +0000

    added the necessary information/details to the about us, Home and team pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,11 +11,15 @@ const Hero = () => {
       <div className="text-white   px-4  ml-0 sm:ml-20    relative  pt-[50px] ">
         <div className="w-[200px] h-[10px] bg-pink-500 absolute right-[130px] sm:top-[110px] top-[180px] -z-0 transform rotate-0  sm:block"></div>
         <h1 className="text-white  font-bold    z-10 text-7xl relative ">
-          Marine <span className="text-yellow-500">Engineering</span>
+        Future Ocean <span className="text-yellow-500">Leaders Academy</span>
         </h1>
         <p className="text-center py-10 text-lg">
-          Dummy text can go here.Dummy text can go here Dummy text can go here
-          Dummy text can go here
+        To inspire and empower the next generation of ocean enthusiasts to become
+        <p> informed decision-makers and advocates for marine environmental protection.</p>
+       
+        </p>
+        <p className="text-center py-10 text-lg">
+        Empowering Tomorrow’s Ocean Leaders Today.
         </p>
       </div>
     </div>
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -7,17 +7,17 @@ import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 export const Teams = () => {
   const members = [
     {
-      name: "Jennifer",
-      title: "Ceo",
+      name: "Dr. Edem Mahu",
+      title: "Director",
       image: teamImg,
       cardColor: "#feb2cc",
       about:
-        "llorem,fsdfs.sdfhuisdf, sdfhsufsdf,suidfsdf,sduihf sdfiusdgfhs fh dsafga fugdsh faksdgh fgags hdfhajshdfh asjdf  uihsdgfhagsdf iauew9 ershdafjsdf",
+        "Dr. Edem Mahu is a Senior Lecturer of Marine Biogeochemistry at the University of Ghana. Her teaching and research span paleoclimatic and environmental reconstructions of marine ecosystems, marine pollution (nutrients, microplastics, heavy metals and their impact on the ocean and human health), marine conservation, and nature-based approaches to coastal challenges. She currently leads the National Geographic Society project “Oyster Reef Restoration and Conservation for Livelihood and Ecosystem Protection in Anyanui” She co-leads the MANCOGA: Mangroves as nature-based solutions to coastal hazards in Ghana project. Dr. Mahu is a Fellow of the Royal Society's Future Leaders African Independent Research (FLAIR) Programme. She is also a research fellow on the African Academy of Sciences' AU-EU ARISE Programme, leading the study Oyster Shell Models for Hindcasting Holocene Environmental Conditions in Equatorial Atlantic Coastal Waters of Africa. She is a member of the trustee board for the partnership for observation of the global oceans, a steering committee member for CoastPredict, a member of the executive board of the Global Ocean Corps and Conveyor, EQUISEA, a member of the academic board for the College of basic and applied sciences academic board at the University of Ghana. She also serves on the board of The Ocean Best Practices system. Dr. Mahu has received several awards and recognitions including being the recipient of the 2022 AGU Africa Award for Research Excellence in Ocean Science. Dr. Mahu is passionate about mentoring the next generation of marine scientists in Africa and has mentored several undergraduates and graduate students in Ghana. She is a current mentor on the National Geographic STEM Field Assistant Program",
     },
 
     {
-      name: "kingsley",
-      title: "software engineer",
+      name: "James",
+      title: "Project Coordinator",
       image: teamImg,
       cardColor: "#c8d6d6",
       about:
@@ -25,15 +25,15 @@ export const Teams = () => {
     },
     {
       name: "Thomas",
-      title: "marketing",
+      title: "Course Instructor",
       image: teamImg,
       cardColor: "#e5dcd3",
       about:
         "llorem,fsdfs.sdfhuisdf, sdfhsufsdf,suidfsdf,sduihf sdfiusdgfhs fh dsafga fugdsh faksdgh fgags hdfhajshdfh asjdf  uihsdgfhagsdf iauew9 ershdafjsdf",
     },
     {
-      name: "nicholas",
-      title: "auditor",
+      name: "Stacy",
+      title: "Tutor",
       image: teamImg,
       cardColor: "#ffcd00",
       about:
@@ -41,7 +41,7 @@ export const Teams = () => {
     },
     {
       name: "Ewurama",
-      title: "accountant",
+      title: "Tutor",
       cardColor: "#ffdebb",
       image: teamImg,
       about:
