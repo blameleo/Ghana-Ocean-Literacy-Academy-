cb7b4b4438249b8eae32cdb81d1904e2b490e5ab
leonardeganow <leonard@eganow.com>
2024-03-17 18:08:26 +0000

    updated about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -137,17 +137,17 @@ function page() {
     },
   ];
   return (
-    <div className=" bg-slate-950 h-screen">
+    <div className=" ">
       <h1 className="sm:text-4xl text-2xl text-center font-bold text-pink-400">
         ABOUT US
       </h1>
       <div className="hidden sm:block">
         <StickyScroll content={content} />
       </div>
-      <TracingBeam className="px-6 block sm:hidden h-screen bg-slate-950">
-        <div className="max-w-2xl mx-auto antialiased pt-4 relative  bg-slate-950">
+      <TracingBeam className="px-6 block sm:hidden h-screen ">
+        <div className="w-full  antialiased pt-4 relative   ">
           {dummyContent.map((item, index) => (
-            <div key={`content-${index}`} className="mb-10 bg-slate-950">
+            <div key={`content-${index}`} className="mb-10  ">
               {item.badge && (
                 <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
                   {item.badge}
@@ -155,8 +155,16 @@ function page() {
               )}
 
               <p className={twMerge("text-xl mb-4")}>{item.title}</p>
+              <Image
+                    src={item.image}
+                    alt="blog thumbnail"
+                    height="1000"
+                    width="1000"
+                    className="rounded-lg mb-10 object-cover"
+                  />
+              <p>{item.description}</p>
 
-              <div className="text-sm  prose prose-sm dark:prose-invert">
+              {/* <div className="text-sm  prose prose-sm prose-invert">
                 {item?.image && (
                   <Image
                     src={item.image}
@@ -167,7 +175,7 @@ function page() {
                   />
                 )}
                 {item.description}
-              </div>
+              </div> */}
             </div>
           ))}
         </div>
--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -227,17 +227,17 @@ function Page() {
     return teams.map((team, index) => (
     
         <CardContainer key={index} className="inter-var sm:w-[26rem] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-700 p-[2px] rounded-xl shadow-lg">
-          <CardBody className=" bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardBody className="  relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2]  w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
             <CardItem
               translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
+              className="text-xl font-bold  text-white"
             >
               {team.badge}
             </CardItem>
             <CardItem
               as="p"
               translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              className=" text-sm max-w-sm mt-2 text-neutral-300"
             >
               {team.title}
             </CardItem>
@@ -254,7 +254,7 @@ function Page() {
             <CardItem
               as="p"
               translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 h-52 overflow-y-scroll "
+              className=" text-sm max-w-sm mt-2 text-neutral-300 h-52 overflow-y-scroll "
             >
               {team.description}
             </CardItem>
